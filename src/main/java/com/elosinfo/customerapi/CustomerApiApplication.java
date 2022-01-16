c8d8a3582f41eacdf0d67e07e87f2b8541aa09a5
Elosinfo <elosinfo21@gmail.com>
2022-01-16 10:16:39 -0300

    Criação da customer controller

--- src/main/java/com/elosinfo/customerapi/CustomerApiApplication.java
+++ src/main/java/com/elosinfo/customerapi/CustomerApiApplication.java
@@ -2,9 +2,16 @@ package com.elosinfo.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerApiApplication {
+public class CustomerApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(CustomerApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApiApplication.class, args);
